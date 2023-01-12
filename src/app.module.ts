cd1695874e130d19d1c1eba234b70c84c63dc1d0
Daria Islanova <dariaislanova@gmail.com>
2023-01-12 20:07:34 +0300

    auth

---
+++ src/app.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import {BookModule} from "./books/book.module";
+import { MongooseModule } from '@nestjs/mongoose';
+import {ConfigModule} from '@nestjs/config'
+import {UserModule} from "./users/user.module";
+
+@Module({
+  imports: [UserModule,BookModule, ConfigModule.forRoot(), MongooseModule.forRoot("mongodb://root:example@mongo:27017/")],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
